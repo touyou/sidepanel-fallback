976704805305ce1e405889ed23f6c7a8392fe406
touyou <fujiyou1121@gmail.com>
2025-07-14 10:11:10 +0900

    chore: remove unnecessary files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,11 +14,14 @@ export default [
       }
     },
     rules: {
-      'no-unused-vars': ['error', { 
-        argsIgnorePattern: '^_', 
-        varsIgnorePattern: '^_', 
-        caughtErrorsIgnorePattern: '^_' 
-      }],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ],
       'no-console': 'warn',
       'no-debugger': 'error',
       'no-undef': 'error',
